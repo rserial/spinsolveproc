03a4e347054b61b6eb23ac07ef81679f82c2c466
Raquel Serial <raquel.serial@tuhh.de>
2024-04-11 16:30:46 +0200

    added PGSTE function to spinsolveproc script

--- src/spinsolveproc/spinsolveproc.py
+++ src/spinsolveproc/spinsolveproc.py
@@ -102,6 +102,7 @@ class SpinsolveExperiment:
             "T2Bulk": process.T2Bulk,
             "T1": process.T1,
             "T1IRT2": process.T1IRT2,
+            "PGSTE": process.PGSTE,
         }
 
         if self.name in processing_functions:

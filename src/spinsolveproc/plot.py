fixed units of saved array

src/spinsolveproc/plot.py
@@ -276,7 +276,7 @@ def setup_fig_PGSTE(
     num_exponentials: Optional[int] = None,
     initial_guesses_expfit: Optional[List[float]] = None,
 ) -> Tuple[go.Figure, go.Figure]:
-    """Set up figures for T1 experiment.
+    """Set up figures for PGSTE experiment.
 
     Args:
         file_path_name (str): The name of the file path.
@@ -521,14 +521,14 @@ def setup_fig_diff_decay_fit(
         err_diffusion_decay.append(err[i * 2 + 1])
 
     trace1_real = go.Scatter(
-        x=diff_scale * 1e-9,
+        x=diff_scale,
         y=np.real(diff_decay) / np.max(np.abs(diff_decay)),
         mode="markers",
         name="T Decay - magnitude",
         marker={"color": "#2C7FB8"},
     )
     trace2 = go.Scatter(
-        x=diff_scale * 1e-9,
+        x=diff_scale,
         y=fitting_kernel(diff_scale, *fitted_parameters[:num_params])
         / np.max(fitting_kernel(diff_scale, *fitted_parameters[:num_params])),
         mode="lines",

src/spinsolveproc/process.py
@@ -266,7 +266,7 @@ def pgste(
 
     ppm_scale = utils.create_ppm_scale(dic)
     grad_scale = np.loadtxt(file_path / "gradients.par")  # in T/m
-    diff_scale = utils.create_diff_scale(dic, grad_scale)
+    diff_scale = utils.create_diff_scale(dic, grad_scale) * 1e-9  # (10⁹ s/m²)
 
     data_2d = np.reshape(data, (grad_scale.shape[0], ppm_scale.shape[0]))
 